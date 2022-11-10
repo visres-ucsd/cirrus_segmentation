e572d017742a65dd81e11a333ceb955d12a434f3
Gonzalez <r2gonzalez@MacBook-Pro-2.attlocal.net>
2022-11-09 22:55:01 -0800

    derived circle scan

--- src/post_process.py
+++ src/post_process.py
@@ -10,7 +10,6 @@ from skimage import morphology
 import argparse
 import scipy
 
-
 MASK_RGB = np.array([133, 248, 208])
 DIST_THRESHOLD = 5000
 EXPECTED_SHAPE = (384, 496)
@@ -132,6 +131,109 @@ def surface_smoothing(layer_df):
     layer_df = cv2.resize(layer_df, (200,200))
     layer_df = pd.DataFrame(layer_df)
     return layer_df
+
+def load_img_data(img_path):
+    return np.fromfile(img_path, dtype=np.uint8).reshape(200, 1024, 200)
+
+def get_projection_image(img_data):
+    projection_image = img_data.mean(axis=1)
+    return projection_image
+
+def get_slab_image(img_data, surface_data_path):
+    # copy array
+    slab_array = np.full(rnfl_surface.shape, np.nan)
+
+    # loop through x, y coords
+    for x in range(0,200):
+        for y in range(0,200):
+            # at each x, y set values outside of slab to nan
+            ilm_idx = ilm_surface.values[x,y]
+            if np.isnan(ilm_idx):
+                continue
+
+            # slice slab and take mean
+            z_idxs = np.arange(0,1024)
+            inside_slab = np.logical_and((z_idxs >= ilm_idx), (z_idxs <= ilm_idx + slab_width))
+            slab_array[x, y] = test_img[x, ~inside_slab, y].mean()
+    return slab_array
+
+EN_FACE_PIX_RESOLUTION = (200/6.) # 200px/6mm
+BASE_DIAM = 3.46 * EN_FACE_PIX_RESOLUTION
+OUTER_RADIUS = (BASE_DIAM + (.1 * EN_FACE_PIX_RESOLUTION)) / 2
+INNER_RADIUS = (BASE_DIAM - (.1 * EN_FACE_PIX_RESOLUTION)) / 2
+
+def derived_circle_scan(vol_data, ilm_surface, rnfl_surface, onh_center, mode):
+    onh_center = np.array(onh_center)
+
+    point = np.array([100,100])
+    coords = np.stack(np.meshgrid(range(200), range(200)), axis=2)
+    coord_diff = coords - point
+    coord_dist = np.sqrt(np.sum((coord_diff)**2, axis=2))
+    coord_mask = np.logical_and(INNER_RADIUS < coord_dist, coord_dist < OUTER_RADIUS)
+    # plt.figure(figsize=(10,10))
+    # plt.imshow(coord_mask)
+
+    # diameter 115px * pi (also converts to degrees easily)
+    anglular_resolution = 360
+    coor_atan = np.arctan2(coord_diff[:,:,0], coord_diff[:,:,1])
+    angle_bins = np.linspace(coor_atan.min(), coor_atan.max(), anglular_resolution)
+    angles_binned = np.digitize(coor_atan, bins=angle_bins).astype(float) - 1
+    bin_labels = np.unique(angles_binned)
+    angles_binned[~coord_mask] = np.nan
+    # plt.figure(figsize=(10,10))
+    # plt.imshow(angles_binned)
+
+    # order bin_labels in TSNIT order
+    if mode=='OD':
+        bin_labels = np.flip((bin_labels + 90) % 360)
+    elif mode=='OS':
+        bin_labels = (bin_labels - 90) % 360
+
+    # debug print out of angular mappings, should follow:
+    # OS: 270 --> 360/0 --> 90 --> 180 --> 270
+    # OD: 90 --> 0/360 --> 270 --> 180 --> 90
+    print(pd.Series({k: v for k, v in enumerate(bin_labels)})[[0, 90, 180, 270, 359]])
+
+    # Take average across 
+    der_circle_scan = []
+    der_ilm_surface = {}
+    der_rnfl_surface = {}
+    for bin_idx, bin_id in enumerate(bin_labels):
+        bin_coords = [i for i in zip(*np.where(angles_binned==bin_id))]
+        bin_lum_values = np.array([vol_data[y, :, x] for y, x in bin_coords])
+        der_circle_scan.append(bin_lum_values.mean(axis=0))
+
+        ilm_values = np.array([ilm_surface[y, x] for y, x in bin_coords])
+        der_ilm_surface[bin_idx] = np.nanmean(ilm_values)
+
+        rnfl_values = np.array([rnfl_surface[y, x] for y, x in bin_coords])
+        der_rnfl_surface[bin_idx] = np.nanmean(rnfl_values)
+
+    der_circle_scan = np.flip(np.array(der_circle_scan).T, axis=0)
+    der_circle_scan = cv2.resize(der_circle_scan, (3*1024, 1024))
+    der_ilm_surface = pd.Series(der_ilm_surface).dropna()
+    der_rnfl_surface = pd.Series(der_rnfl_surface).dropna()
+
+    der_ilm_surface.index = der_ilm_surface.index * der_circle_scan.shape[1] / bin_labels.size
+    der_rnfl_surface.index = der_rnfl_surface.index * der_circle_scan.shape[1] / bin_labels.size
+    
+    plt.figure(figsize=(10,5))
+    plt.imshow(der_circle_scan, cmap='gray', aspect='auto')
+
+    ilm_x_res = np.linspace(0, der_circle_scan.shape[1], der_ilm_surface.size)
+    der_ilm_surface.plot(linewidth=1, color='cyan')
+    der_rnfl_surface.plot(linewidth=1, color='r')
+
+    # set ticks to scale
+    xticks = np.array([0, 90, 180, 270, 360])
+    xticks_pos = xticks * (der_circle_scan.shape[1] / 360)
+    yticks = np.array([0, 1, 2, 3, 4, 5, 6])
+    yticks_pos = yticks * (der_circle_scan.shape[0] / 6)
+    plt.xticks(xticks_pos, ['T', 'S', 'N', 'I', 'T'])
+    plt.yticks(yticks_pos, yticks)
+
+    plt.ylabel('Millimeters (mm)')
+    return der_circle_scan
 
 def main():
     '''
