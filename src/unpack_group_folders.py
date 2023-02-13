8573d60f2fe16c1ba060c81de9213f25380c5db0
Gonzalez <r2gonzalez@MacBook-Pro-2.attlocal.net>
2023-02-13 02:44:31 -0800

    optimize by folder

--- src/unpack_group_folders.py
+++ src/unpack_group_folders.py
@@ -7,11 +7,11 @@ def main():
     parser.add_argument('dir')
     args = parser.parse_args()
 
-    for fp in tqdm(list(Path(args.dir).rglob('*.jpg'))):
+    for fp in tqdm(list(Path(args.dir).glob('*/*/*'))):
         parts = list(fp.parts)
         parts = [i for i in parts if not (i=='zhi_controls' or i=='zhi_cases')]
         renamed_path = Path().joinpath(*parts)
-        renamed_path.parent.mkdir(parents=True, exist_ok=True)
+        renamed_path.mkdir(parents=True, exist_ok=True)
         fp.rename(renamed_path)
 
 if __name__=='__main__':
