71a27928fd900cf3ca3618fde9f8e400778f0444
Gonzalez <r2gonzalez@MacBook-Pro-2.attlocal.net>
2023-02-13 03:15:38 -0800

    resume fix

--- src/unpack_group_folders.py
+++ src/unpack_group_folders.py
@@ -7,7 +7,7 @@ def main():
     parser.add_argument('dir')
     args = parser.parse_args()
 
-    for fp in tqdm(list(Path(args.dir).glob('*/*/*'))):
+    for fp in tqdm(list(Path(args.dir).glob('zhi*/*/*'))):
         parts = list(fp.parts)
         parts = [i for i in parts if not (i=='zhi_controls' or i=='zhi_cases')]
         renamed_path = Path().joinpath(*parts)
